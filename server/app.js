1f3f78a3e0bb50e406d5f8999fc6d1faae86e617
kiddliam <vansfreelancer@gmail.com>
2024-02-11 12:53:28 +0800

    Connect to database

--- server/app.js
+++ server/app.js
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(`Connection error : ${err}`);
+  });
 
 app.listen(3000, () => {
   console.log(`Server running at http://localhost:3000`);
